2ef4464bca4da1ee845759d56f4eab8b0d950323
Dzil <ahmadfadlydziljalal@gmail.com>
2022-06-08 19:51:08 +0700

    Remove plain folder and add build-production

--- source/blog/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('_layouts.main')
-
-@section('body')
-<div class="container-fluid">
-    <div class="flex justify-content-center" style="height: 100%;">
-        <h1>500 | Blog page is under construction</h1>
-    </div>
-</div>
-@endsection
