c7a1b16c3b5b1c0e84b7a25e130c7f7a4c5adb08
Dzil <ahmadfadlydziljalal@gmail.com>
2022-06-09 12:12:26 +0700

    Tambah css untuk hover

--- config.php
+++ config.php
@@ -1,9 +1,16 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
     'production' => false,
+    'company' => ' CoPasModifDev.',
     'baseUrl' => '',
     'title' => 'CoPasModifDev.',
     'description' => 'Portofolio & Docs',
+    'contact_email' => 'ahmadfadlydziljalal@gmail.com',
     'collections' => [],
+    'selected' => function($page, $section){
+        return Str::contains($page->getPath(), $section) ? 'active' : '';
+    }
 ];
